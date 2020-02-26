843e9d7779020509c5d45efea4422f213068cf8c
Eric Scott <eric.scott@acm.org>
2020-02-26 07:00:03 -0800

    Updating versions of dependencies per lein ancient

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-options {:init-ns sparql-endpoint.core}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [clj-http "3.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
+                 [clj-http "3.10.0"]
                  ;; logging...
                  [com.taoensso/timbre "4.10.0"]
                  ;; fixes a warning in logging...
-                 [com.fzakaria/slf4j-timbre "0.3.8"]
+                 [com.fzakaria/slf4j-timbre "0.3.19"]
                  ;; included per docs in slf4j-timbre...
-                 [org.slf4j/log4j-over-slf4j "1.7.14"]
-                 [org.slf4j/jul-to-slf4j "1.7.14"]
-                 [org.slf4j/jcl-over-slf4j "1.7.14"]
+                 [org.slf4j/log4j-over-slf4j "1.7.30"]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]
                  ;; makes light use of jena to parse SPARQL, XSD values, etc...
-                 [org.apache.jena/jena-core "3.6.0"]
-                 [org.apache.jena/jena-arq "3.6.0"]
-                 [org.apache.jena/jena-iri "3.6.0"]
+                 [org.apache.jena/jena-core "3.14.0"]
+                 [org.apache.jena/jena-arq "3.14.0"]
+                 [org.apache.jena/jena-iri "3.14.0"]
                  ]
   :plugins [[lein-codox "0.10.6"]
             ]
