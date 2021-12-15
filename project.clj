d1892d98b2b700c96fa12fc864488ac65dd47fd7
Eric Scott <eric.scott@acm.org>
2021-12-15 08:46:29 -0800

    v 0.1.4 - Refreshing the deps

--- project.clj
+++ project.clj
@@ -1,36 +1,28 @@
 
-(defproject ont-app/sparql-endpoint "0.1.3-SNAPSHOT"
+(defproject ont-app/sparql-endpoint "0.1.4"
   :description "Utilities for interfacing with a sparql endpoint in clojure"
   :url "https://github.com/ont-app/sparql-endpoint/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-options {:init-ns sparql-endpoint.core}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/data.json "2.3.1"]
-                 [clj-http "3.12.2"]
+  :dependencies [;; deps tree ambiguities
+                 [commons-io "2.11.0"]
+                 [org.slf4j/slf4j-api "1.7.32"]
+                 ;; clojure
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [clj-http "3.12.3"]
                  ;; logging...
                  [com.taoensso/timbre "5.1.2"]
                  ;; fixes a warning in logging...
-                 [com.fzakaria/slf4j-timbre "0.3.21"
-                  :exclusions [[org.slf4j/slf4j-api]]]
-                 ;; included per docs in slf4j-timbre...
-                 [org.slf4j/log4j-over-slf4j "1.7.31"] ;;"1.7.30"]
-                 [org.slf4j/jul-to-slf4j "1.7.31"]
-                 [org.slf4j/jcl-over-slf4j "1.7.31"]
+                 [com.fzakaria/slf4j-timbre "0.3.21"]
                  ;; makes light use of jena to parse SPARQL, XSD values, etc...
-                 [org.apache.jena/jena-core "3.14.0"] 
-                 [org.apache.jena/jena-arq "3.14.0"]
-                 [org.apache.jena/jena-iri "3.14.0"]
+                 [org.apache.jena/jena-core "4.3.1"] 
+                 [org.apache.jena/jena-arq "4.3.1"] 
+                 [org.apache.jena/jena-iri "4.3.1" ]
                  ;; ... see Appendix below for notes about jena versioning
-                 [ont-app/vocabulary "0.1.3"
-                  :exclusions
-                  [
-                   [com.google.code.findbugs/jsr305]
-                   [com.google.errorprone/error_prone_annotations]
-                   [lein-doo]
-                   [org.clojure/clojurescript]
-                  ]
-                 ]]
+                 [ont-app/vocabulary "0.1.5"]
+                 ]
   :plugins [[lein-codox "0.10.6"]
             ]
   :codox {:output-path "doc"}
